9970234883cb2c8a0dd68ac2c4c02f710b73d287
sergey-tomin <tomin.sergey@gmail.com>
2023-02-07 11:25:22 +0100

    replaced font folder and js folder to assets

--- sw.js
+++ sw.js
@@ -15,25 +15,23 @@ const CACHE = CACHE_NAMESPACE + 'precache-then-runtime';
 const PRECACHE_LIST = [
   "./",
   "./offline.html",
-  "./js/jquery.min.js",
-  "./js/bootstrap.min.js",
-  "./js/hux-blog.min.js",
-  "./js/snackbar.js",
+  "./assets/js/jquery.min.js",
+  "./assets/js/bootstrap.min.js",
+  "./assets/js/hux-blog.min.js",
+  "./assets/js/snackbar.js",
   "./img/icon_tt.png",
   //"./img/avatar-hux.jpg",
   "./img/home-bg.jpeg",
   "./img/404-bg.jpg",
-  "./css/hux-blog.min.css",
-  "./css/bootstrap.min.css"
+  "./assets/css/hux-blog.min.css",
+  "./assets/css/bootstrap.min.css"
   // "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css",
   // "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/fonts/fontawesome-webfont.woff2?v=4.6.3",
   // "//cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.6/fastclick.min.js"
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
-  "yanshuo.io",
-  "cdnjs.cloudflare.com"
+  "tomintales.com",
 ]
 const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
 
